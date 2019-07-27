0b9e954cbe22338649840f9df94d572a1903c5e4
alberti-tu <albertalvarezmiron@gmail.com>
2019-07-27 22:20:33 +0200

    Final Commit

--- ElderPi-server/app.js
+++ ElderPi-server/app.js
@@ -2,6 +2,7 @@
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 const path = require('path');
 
 // Configuration
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
 
 // Server inicialization
 const http = require('http').createServer(app).listen(config.http.port);
---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+if [ $(id -u) -ne 0 ] ; then echo "Please run as root" ; exit 1 ; fi
+
+echo "Updating packages..."
+apt update -y
+apt upgrade -y
+
+echo "Installing NodeJS and NPM..."
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt install nodejs -y
+
+echo "Updating NPM and Angular to the latest version..."
+npm install -g npm
+npm install -g @angular/cli
+
+echo "Installing programs..."
+apt install mariadb-server screen -y
+mysql --user="root" --database="mysql" --execute="update user set plugin='' where User='root'; flush privileges;"
+
+echo "Changing the Network Manager"
+apt install network-manager network-manager-gnome openvpn \openvpn-systemd-resolved network-manager-openvpn \network-manager-openvpn-gnome -y
+apt purge openresolv dhcpcd5 -y
+ln -sf /lib/systemd/resolv.conf /etc/resolv.conf
+
+echo "Cleaning packages..."
+apt clean -y
+apt autoremove -y
+
+cd ElderPi-client
+npm install
+cd ..
+
+cd ElderPi-server
+npm install
+cd ..
+
+cd ElderPi-server/certificate
+openssl req -nodes -new -x509 -keyout server.key -out server.cert
+cd ../..
+
+echo "Process finished!"
+echo "Now reboot..."
+
+reboot
+
+
