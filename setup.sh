6427cdf42e90bb7daec1b01f5596ab635618f23a
alberti-tu <albertalvarezmiron@gmail.com>
2019-11-10 19:54:19 +0100

    Access Point

--- setup.sh
+++ setup.sh
@@ -1,44 +1,61 @@
 #!/bin/bash
 if [ "$(id -u)" -ne 0 ] ; then echo "Please run as root" ; exit 1 ; fi
 
-echo "Updating packages..."
+# Updating packages...
 apt update -y
 apt upgrade -y
 
-echo "Installing NodeJS and NPM..."
+# Installing NodeJS and NPM...
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt install nodejs -y
 
-echo "Updating NPM and Angular to the latest version..."
+# Updating NPM and Angular to the latest version...
 npm install -g npm
 npm install -g @angular/cli
 
-echo "Installing programs..."
-apt install mariadb-server screen -y
+# Installing programs...
+apt install mariadb-server screen dnsmasq hostapd -y
+
+# Root permisions for database access
 mysql --user="root" --database="mysql" --execute="update user set plugin='' where User='root'; flush privileges;"
 
+# Stop services for Access Point
+systemctl stop dnsmasq
+systemctl stop hostapd
+
+# Configuring static IP
+cp config/dhcpcd.conf /etc/dhcpcd.conf
+service dhcpcd restart
+
+# Configuring DHCP server
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+cp config/dnsmasq.conf /etc/dnsmasq.conf
+systemctl reload dnsmasq
+
+# Configuring Wi-Fi atributes...
+cp config/hostapd.conf /etc/hostapd/hostapd.conf
+cp config/hostapd /etc/default/hostapd
+
+# Starting services for Acces Point
+systemctl unmask hostapd
+systemctl enable hostapd
+systemctl start hostapd
+
+# Compiling client...
 cd ElderPi-client || exit
 npm install
 npm run-script build
 cd ..
 
+# Compiling server...
 cd ElderPi-server || exit
 npm install
 mkdir certificate
 cd ..
 
+# Generating SSL certificates...
 openssl req -nodes -new -x509 -keyout ElderPi-server/certificate/server.key -out ElderPi-server/certificate/server.cert -days 365
 
-echo "Changing the Network Manager"
-apt install network-manager network-manager-gnome openvpn \openvpn-systemd-resolved network-manager-openvpn \network-manager-openvpn-gnome -y
-apt purge openresolv dhcpcd5 -y
-ln -sf /lib/systemd/resolv.conf /etc/resolv.conf
-
-echo "Cleaning packages..."
+# Cleaning packages...
 apt clean -y
 apt autoremove -y
-
-echo "Process finished!"
-echo "Now reboot..."
-
-reboot
